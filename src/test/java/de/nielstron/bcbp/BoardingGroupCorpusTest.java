eb2ea4e7226a710555bb6fcf030a49758fb8d38a
Niels <niels.muendler@inf.ethz.ch>
2026-05-18 08:46:23 +0200

    Add Kitinerary BCBP corpus fixtures

--- src/test/java/de/nielstron/bcbp/BoardingGroupCorpusTest.java
+++ src/test/java/de/nielstron/bcbp/BoardingGroupCorpusTest.java
@@ -35,6 +35,110 @@ class BoardingGroupCorpusTest {
             null
         ),
         new BoardingGroupSample(
+            "KItinerary easyJet sample",
+            "M1DOE/JOHN            EABCDEFGMRSLGWEZY8724 99  3C  506  10Axxxxxxxxxx",
+            "EZY",
+            "506",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary IATA Resolution 792 example 1",
+            "M1DESMARAIS/LUC       EABC123 YULFRAAC 0834 326J001A0025 100",
+            "AC",
+            "25",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary IATA Resolution 792 example 2",
+            "M1DESMARAIS/LUC       EAB12C3 YULFRAAC 0834 326J003A0027 167>5321WW1325BAC 0014123456002001412346700100141234789012A0141234567890 1AC AC 1234567890123    4PCYLX58Z^108ABCDEFGH",
+            "AC",
+            "27",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary IATA Resolution 792 example 3",
+            "M1GRANDMAIRE/MELANIE  EABC123 GVACDGAF 0123 339C002F0025 130>5002A0571234567890  AF AF 1234567890123456    Y^108ABCDEFGH",
+            "AF",
+            "25",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary IATA Resolution 792 example 4, first leg",
+            "M2DESMARAIS/LUC       EAB12C3 YULFRAAC 0834 326J003A0027 167>5321WW1325BAC 0014123456002001412346700100141234789012A0141234567890 1AC AC 1234567890123    4PCYLX58ZDEF456 FRAGVALH 3664 327C012C0002 12E2A0140987654321 1AC AC 1234567890123    3PCNWQ^108ABCDEFGH",
+            "AC",
+            "27",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary IATA Resolution 792 example 5, first leg",
+            "M2GRANDMAIRE/MELANIE  EABC123 GVACDGAF 0123 339C002F0025 130>5002A0571234567890  AF AF 1234567890123456    YDEF456 CDGDTWNW 0049 339F001A0002 12C2A012098765432101                       2PC ^108ABCDEFGH",
+            "AF",
+            "25",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary issue-date sample",
+            "M1DOE/JOHN            EXXX007 TXLBRUSN 2588 034Y023D0999 35D>5181WM7034BSN              2A08200000000000 SN LH 123456789012345      *30600000K0902       ",
+            "SN",
+            "999",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary minimal sample",
+            "M1DOE/JOHN            EXXX007 TXLBRUSN 2592 110Y",
+            "SN",
+            "",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary minimal sample without compartment",
+            "M1DOE/JOHN            EXXX007 TXLBRUSN 2592 110",
+            "SN",
+            "",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary missing e-ticket indicator sample",
+            "M1DOE/JOHN             XXX007 BRUTXLEW 8103 035Y012C0030 147>1181W 8033BEW 0000000000000291040000000000 0   LH 123456789012345     ",
+            "EW",
+            "30",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary Qatar zero-size conditional section sample",
+            "M1DOE/JANE            EXXX007 MXPDOHQR 0128 256Y042F0023 100>2180  0255BBR              2963456000789980                            0",
+            "QR",
+            "23",
+            ">2180  0255BBR              2963456000789980                            0",
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary rail-coded LH sample",
+            "M1DRAGON/KONQI DR     EXXX007 XHJFRALH 3489 129M092H0002 359>6180WM4128BLH              2A22012345678900 LH                        N*30600000K05     ",
+            "LH",
+            "2",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
+            "KItinerary TAP missing issue-date sample",
+            "M1DOE/JOHN            EXXX007 LISLCGTP 1080 204Y002D0003 35C>2180      B1A              2904712345678900                           *306      09     BRND",
+            "TP",
+            "3",
+            null,
+            null
+        ),
+        new BoardingGroupSample(
             "Synthetic United-style experiment, visible pass says Group 2",
             minimalBcbp("UA", "00042", "BG:2"),
             "UA",
